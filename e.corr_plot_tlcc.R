73dd53beae36d970fdc741962bfa8e70188aa78f
mmorenozam <mmorenozambrano@gmail.com>
2022-01-17 14:17:12 +0100

    17.01.2022

--- e.corr_plot_tlcc.R
+++ e.corr_plot_tlcc.R
@@ -7,19 +7,60 @@ out_plots <- paste0(wd,'/figures/')
 w_data <- paste0(wd,'/working_data/')
 
 rot <- read.csv(paste0(w_data,'sw_corr_rot_no_weeks.csv'))
-rot1 <- rot
-rot1$val[rot1$val>0.05]<-NA
-head(rot)
+cam <- read.csv(paste0(w_data,'sw_corr_cam_no_weeks.csv'))
+inf <- read.csv(paste0(w_data,'sw_corr_inf_no_weeks.csv'))
+bor <- read.csv(paste0(w_data,'sw_corr_bor_no_weeks.csv'))
 
-p1 <- ggplot(rot1,aes(x=lag,y=window))+
-  geom_raster(aes(fill=log(as.numeric(val))))+
-  scale_fill_viridis_c(option="C")+
-  facet_wrap(~wc)+
-  theme_bw()+
-  theme(legend.position="bottom")
+sub_fun <- function(df){
+  df <- subset(df,wc!="Max. Temp"&wc!="Min. Temp"&wc!="hdd"&wc!="cdd")
+  return(df)
+}
 
-save_plot(paste0(out_plots,"rot_pval_abs.pdf"),p1,base_height = 9,base_width = 12)
+rot <- sub_fun(rot)
+cam <- sub_fun(cam)
+inf <- sub_fun(inf)
+bor <- sub_fun(bor)
 
+
+plt_fun <- function(df,th,n){
+  df$val[df$val>th] <- NA
+  p1 <- ggplot(df,aes(x=lag,y=window/7))+
+    geom_raster(aes(fill=log(as.numeric(val))))+
+    scale_fill_viridis_c(option="C")+
+    facet_wrap(~wc)+
+    theme_bw()+
+    theme(legend.position="bottom")+
+    ylab("window (weeks)")+
+    xlab("lag (weeks)")+
+    guides(fill=guide_legend(title="log(p-value)"))
+  
+  save_plot(paste0(out_plots,n,"_pval_abs.pdf"),p1,base_height = 9,base_width = 12) 
+}
+
+plt_fun(rot,0.005,'rot')
+plt_fun(cam,0.005,'cam')
+plt_fun(inf,0.005,'inf')
+plt_fun(bor,0.005,'bor')
+
+plt_fun_corr <- function(df,th,n){
+  df$corr[df$val>th] <- NA
+  p1 <- ggplot(df,aes(x=lag,y=window))+
+    geom_raster(aes(fill=corr))+
+    scale_fill_viridis_c(option="C")+
+    facet_wrap(~wc)+
+    theme_bw()+
+    theme(legend.position="bottom")+
+    ylab("window (weeks)")+
+    xlab("lag (weeks)")+
+    guides(fill=guide_legend(title="correlation"))
+  
+  save_plot(paste0(out_plots,n,"_corrs_abs.pdf"),p1,base_height = 9,base_width = 12) 
+}
+
+plt_fun_corr(rot,0.005,'rot')
+plt_fun_corr(cam,0.005,'cam')
+plt_fun_corr(inf,0.005,'inf')
+plt_fun_corr(bor,0.005,'bor')
 
 ###check this paper at home
 # Temperature-dependent transmission
